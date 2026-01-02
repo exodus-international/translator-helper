fcadabbe1fdca62e76348c6b9a3a46bb95ae061a
Vojtech Cerveny <cervik49@gmail.com>
2026-01-02 14:22:11 +0100

    feat: integrate Sentry for error tracking and monitoring, including configuration for server and edge environments, and update .gitignore and package.json for new dependencies

---
+++ src/instrumentation-client.ts
@@ -0,0 +1,17 @@
+// This file configures the initialization of Sentry on the client.
+// The added config here will be used whenever a users loads a page in their browser.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from '@sentry/nextjs';
+
+Sentry.init({
+  dsn: 'https://04c1d5d3401cd052052fa119c1dca49d@o485634.ingest.us.sentry.io/4510640872357888',
+  // Enable logs to be sent to Sentry
+  enableLogs: true,
+
+  // Enable sending user PII (Personally Identifiable Information)
+  // https://docs.sentry.io/platforms/javascript/guides/nextjs/configuration/options/#sendDefaultPii
+  sendDefaultPii: true,
+});
+
+export const onRouterTransitionStart = Sentry.captureRouterTransitionStart;
